690ee1543e3e89303dcb81d2beb6e13eb5ef211b
sara-yifrach <sarayifrach177@gmail.com>
2024-09-29 12:28:32 +0300

    Initial commit

---
+++ WebApplication1/Controllers/TimerController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimerExample.Controllers
+{
+    [Route("timer")]
+    public class TimerController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetTimer()
+        {
+            Response.Headers.Add("Content-Type", "text/event-stream");
+
+            var responseStream = Response.Body;
+
+            while (true)
+            {
+                var data = Encoding.UTF8.GetBytes($"data: {DateTime.Now.ToString("HH:mm:ss")}\n\n");
+                await responseStream.WriteAsync(data);
+                await responseStream.FlushAsync();
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
